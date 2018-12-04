c79ec38000e9fad8bbfa31965d930b308e194032
Your Name <you@example.com>
2018-12-05 00:09:41 +0200

    Initial start of the project

---
+++ src/application/FixedLengthStringIO.java
@@ -0,0 +1,6 @@
+package application;
+import java.io.*;
+public class FixedLengthStringIO {
+	public static String readFixedLengthString(int size, DataInput in) throws IOException{return null;}
+	public static void writeFixedLengthString(String s, int size, DataOutput out) throws IOException {}
+}
